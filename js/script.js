5549d11d3c3ca7e657995d11a58f82955369a75f
itsalle <paulaalemand@gmail.com>
2019-11-05 21:45:40 -0300

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+// Declarando variáveis
+// Contar alguma coisa
+
+var variavelUm;
+let variavelDois;
+const variavelTres = "Você é legal, cara"; // atribua valor para sua const quando declará-la, pô
+
+// Atribuição
+
+var variavelQuatro = 12;
+var atomEuTeAmo = "Não liga pra eles";
+var henriqueNaoEMeuAmigo = false;
+
+// Tipos de Dados
+// bizuzaoDoMal = variavelQuatro.minhaFuncao();
+
+let bizuzaoDoMal;
+bizuzaoDoMal = "Beterraba"; //String
+bizuzaoDoMal = 17.3; // Number
+bizuzaoDoMal = 42; // Number
+bizuzaoDoMal = true; // Boolean
+bizuzaoDoMal = false; // Boolean
+
+let docesDeFrutas = []; // Lista
+
+docesDeFrutas = ["goiaba", "cocada", "abóbora", "banana", "morango", "maracujá", "limão"]; // Lista
+
+let cris = ["Enter", 23, false, "Empreendedor", "Cohab2", ["meigo", "divertido", "caozeiro", "palhaço"]]; // Lista
+
+let renata = {
+    sobrenome: "Ingrata",
+    idade: 21,
+    viva: true,
+    bairro: "Bela Vista",
+    hobbies: ["trocar o amor por uma ilusão", "fazer trilha", "jogar o joguinho"]
+}; // Objeto
+
+let  henriqueHenriqueFeatDrake = {
+    primeiraMusica: "Ninjei",
+    futuroFeat: "Pabllo Vittar e Caneta Azul, part Moleko",
+    primeiroShow: "Errejota",   
+    idioma: "Coreano",
+    dia: 29,
+    legais: true
+}; // Objeto
+
+// Revisona
+
+// Condição
+
+if (se)
+else (senão)
+else if (e senão)
+while (enquanto)
+
+if(condição){
+    o que eu quero que aconteça caso a condição seja verdeira
+} else {
+    o que eu quero que aconteça caso a condição seja falsa
+}
+
+if(condição){
+    o que eu quero que aconteça caso a primeira condição seja verdeira
+} else if (condição) {
+    o que quero que aconteça caso a minha primeira condição seja falsa e a segunda condição verdadeira
+} else {
+    o que eu quero que aconteça caso todas as condições sejam falsas
+}
+
+
